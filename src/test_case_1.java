f637e6aabb152539cdcf497704dd1c5ba86db50f
TAPOSHDAS <44937930+TAPOSHDAS@users.noreply.github.com>
2023-12-23 22:41:25 +0600

    Add test case 1

--- src/test_case_1.java
+++ src/test_case_1.java
@@ -1,27 +1,37 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class test_case_1 {
-
+public class test_case_1{
     public static void main(String[] args) {
 
+//        System.out.printf("Hello and welcome!");
+
+
+        //Launch or open the  chrome browser
         System.setProperty("webdriver.chrome.driver",
                 "C:\\Users\\88016\\Desktop\\cv\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
+//        ChromeDriverService service = new ChromeDriverService.Builder().build();
+//        WebDriver driver = new ChromeDriver(service);
+
+        //launch webpage or navigate to the url
+        driver.navigate().to("https://www.saucedemo.com/");
+
+        //capture title of webpage
+        String title=driver.getTitle();
+        System.out.println("Page Title: "+title);
+
+        //capture url of the webpage
+        System.out.println("Url: "+driver.getCurrentUrl());
+
+        //Capture page source
+        System.out.println("Page Source: "+driver.getPageSource());
+
+        //close the webpage
+        driver.close();
 
 
-        driver.navigate().to("https://www.saucedemo.com/v1/");
 
-
-        driver.findElement(By.cssSelector("input#user-name")).sendKeys("standard_user\n");
-
-
-        driver.findElement(By.cssSelector("input[name=password]")).sendKeys("secret_sauce");
-
-
-
-        driver.findElement(By.cssSelector("input.btn_action")).click();
 
     }
 }
