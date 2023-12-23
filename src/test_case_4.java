879fd8fa4387bb6553b99ea34b8729df8c1834a8
TAPOSHDAS <44937930+TAPOSHDAS@users.noreply.github.com>
2023-12-23 22:42:47 +0600

    Add test case 4

---
+++ src/test_case_4.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test_case_4{
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\88016\\Desktop\\cv\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+
+
+        driver.navigate().to("https://www.saucedemo.com/v1/");
+
+
+        driver.findElement(By.cssSelector("input#user-name")).sendKeys("standard_user\n");
+
+
+        driver.findElement(By.cssSelector("input[name=password]")).sendKeys("");
+
+
+
+        driver.findElement(By.cssSelector("input.btn_action")).click();
+
+    }
+}
+
+
+
+
+
